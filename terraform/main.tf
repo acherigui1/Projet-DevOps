92aba187ac74f6024a6f22e49189c4117086be0f
amine <aminecheriguifr@gmail.com>
2023-12-04 10:21:53 +0100

    first commit

---
+++ terraform/main.tf
@@ -0,0 +1,50 @@
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+# Main resource group
+resource "azurerm_resource_group" "rg_main" {
+  name     = var.resource_group
+  location = var.location
+  tags = {
+    environment = "Terraform Lab"
+  }
+}
+
+# Créez un registre de conteneurs Azure
+resource "azurerm_container_registry" "acr" {
+  name                = "amineContainerRegistry"
+  resource_group_name = azurerm_resource_group.rg_main.name
+  location            = azurerm_resource_group.rg_main.location
+  sku                 = "Standard"
+  admin_enabled       = true
+}
+
+# Créez un cluster Kubernetes Service
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "amineAKSCluster"
+  location            = azurerm_resource_group.rg_main.location
+  resource_group_name = azurerm_resource_group.rg_main.name
+  dns_prefix          = "myakscluster"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
---
+++ terraform/terraform.tfvars
@@ -0,0 +1,3 @@
+# Resource Group/Location
+location             = "francecentral"
+resource_group       = "rg-amine-cherigui-cc"
